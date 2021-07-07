b7b027377c5b21de0db6d487a08b983e239453c3
Deepanjan Nag <subscriptions@deepanjannag.com>
2021-07-07 23:59:29 +0100

    some errors. but progress made!

--- js/script.js
+++ js/script.js
@@ -1,15 +1,91 @@
-let addItemButton = document.querySelector('form');
+const form = document.querySelector('#itemForm'),
+    itemInput = document.querySelector('#itemInput'),
+    itemList = document.querySelector('.item-list'),
+    feedback = document.querySelector('.feedback'),
+    clearButton = document.querySelector('#clear-list');
 
-addItemButton.addEventListener('submit', e => {
+let todoItems = [];
+
+function getLocalStorage() {
+    const todoStorage = localStorage.getItem('todoItems');
+    if (todoStorage) {
+        todoItems = JSON.parse(todoStorage);
+        getList(todoItems);
+    }
+    else
+        todoItems = [];
+}
+
+function setLocalStorage(todoItems) {
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+}
+getLocalStorage();
+
+form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    let todoItem = document.querySelector('#itemInput').value;
+    const itemName = itemInput.value;
 
-    document.querySelector('#itemInput').value = '';
-
-    let todoElement = document.createElement('div');
-    todoElement.innerHTML = todoItem;
-
-    document.querySelector('.item-list').appendChild(todoElement);
-
+    if (itemName.length == 0) {
+        feedback.innerHTML = 'Please Enter Valid Value';
+        feedback.classList.add('showItem', 'alert-danger');
+        setTimeout(() => {
+            feedback.classList.remove('showItem');
+        }, 3000);
+    }
+    else {
+        todoItems.push(itemName);
+        setLocalStorage(todoItems);
+        getList(todoItems);
+        //add event listeners to icons;
+        //handleItem(itemName);
+    }
+    itemInput.value = '';
 });
+
+clearButton.addEventListener('click', () => {
+    todoItems = [];
+    localStorage.clear();
+    getList(todoItems);
+});
+
+function getList(todoItems) {
+    //itemList.innerHTML = '';
+
+    todoItems.forEach(item => {
+        itemList.insertAdjacentHTML("beforeend", `<div class="item my-3"><h5 class="item-name text-capitalize">${ item }</h5><div class="item-icons"><a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a><a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a><a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a></div></div>`);
+
+        handelItem(item);
+    });
+}
+
+function handelItem(itemName) {
+
+    let items = itemList.querySelectorAll('.item');
+
+    items.forEach(item => {
+        if (item.querySelector('.item-name').textContent == itemName) {
+
+            //COMPLETED-ITEM icon is clicked
+            item.querySelector('.complete-item').addEventListener('click', function () {
+                item.querySelector('.item-name').classList.toggle('completed');
+                this.classList.toggle('visibility');
+            });
+            //EDIT icon is clicked
+            item.querySelector('.edit-item').addEventListener('click', function () {
+                itemInput.value = itemName;
+                itemList.removeChild(item);
+
+                todoItems = todoItems.filter(i => i != itemName);
+            });
+            //DELETE icon is clicked
+            item.querySelector('.delete-item').addEventListener('click', function () {
+                itemList.removeChild(item);
+                todoItems = todoItems.filter(i => i != itemName)
+            });
+
+            //showFeedback('item delete', 'success');
+        }
+    });
+}
+
