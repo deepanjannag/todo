7e017ea7a49f4efef81bfb1f6a78a90148ed7634
Deepanjan Nag <subscriptions@deepanjannag.com>
2021-07-08 16:26:38 +0100

    Added a comment

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ function createScreenList(todoItems) {
     todoItems.forEach(item => {
         itemList.insertAdjacentHTML("beforeend", `<div class="item my-3"><h5 class="item-name text-capitalize">${ item }</h5><div class="item-icons"><a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a><a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a><a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a></div></div>`);
 
-        let tobeHandledItem = itemList.lastChild;
+        let tobeHandledItem = itemList.lastChild;   //We're choosing the above added html!
 
         //COMPLETED
         tobeHandledItem.querySelector('.complete-item').addEventListener('click', () =>
